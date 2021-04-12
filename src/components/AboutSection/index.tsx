9dd1d2e68a3909a772b7e222425e8c5f3174fd1d
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-04-12 23:09:39 +0100

    adding skills

--- src/components/AboutSection/index.tsx
+++ src/components/AboutSection/index.tsx
@@ -9,12 +9,70 @@ function AboutSection() {
         <Col xs={6} className="bioHalf pt-5">
           <p>Good morning and/or evening! </p>
           <p>
-            My name is Abdul Mahmood, and I'm a London-based, MERN stack, web
-            developer. While
+            My name is Abdul Mahmood, and I'm a
+            <strong> London-based, MERN stack, web developer</strong>. While I
+            may be living in London, I am fully ready for
+            <strong> remote working.</strong>
           </p>
+          <p></p>
         </Col>
         <Col xs={6} className="skillHalf pt-5">
-          SKILLS HERE
+          <Row>
+            <Col xs={6}>
+              <p>
+                <strong>Languages - Human</strong>
+              </p>
+              <ul>
+                <li>English</li>
+              </ul>
+            </Col>
+            <Col xs={6}>
+              <p>
+                <strong>Languages - Code</strong>
+              </p>
+              <ul>
+                <li>JavaScript</li>
+                <li>HTML5</li>
+                <li>CSS</li>
+                <li>SQL</li>
+              </ul>
+            </Col>
+          </Row>
+          <Row>
+            <Col xs={4}>
+              <p>
+                <strong>Skills - Human</strong>
+              </p>
+              <ul>
+                <li>Pair Programming</li>
+                <li>Agile</li>
+                <li>SCRUM</li>
+                <li>Instant Coffee</li>
+              </ul>
+            </Col>
+            <Col xs={4}>
+              <p>
+                <strong>Skills - Backend</strong>
+              </p>
+              <ul>
+                <li>JavaScript</li>
+                <li>HTML5</li>
+                <li>CSS</li>
+                <li>SQL</li>
+              </ul>
+            </Col>
+            <Col xs={4}>
+              <p>
+                <strong>Skills - Frontend</strong>
+              </p>
+              <ul>
+                <li>JavaScript</li>
+                <li>HTML5</li>
+                <li>CSS</li>
+                <li>SQL</li>
+              </ul>
+            </Col>
+          </Row>
         </Col>
       </Row>
     </Container>
