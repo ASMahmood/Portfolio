0595d1c876f5eb96179ecf85806e37535ba12892
Abdul Samad Mahmood <72197815+ASMahmood@users.noreply.github.com>
2021-04-13 01:24:37 +0100

    styling cards

--- src/components/SoloProjects/index.tsx
+++ src/components/SoloProjects/index.tsx
@@ -6,21 +6,23 @@ import "./style.css";
 
 function SoloProjects(props: soloProject) {
   return (
-    <Card className="soloCard">
+    <Card bg="none" text="light" className="soloCard">
       <Card.Img variant="top" className="projectPreview" src={props.preview} />
       <Card.Body>
         <Card.Title>{props.name}</Card.Title>
-        <Card.Text>{props.description}</Card.Text>
-        <div className="d-flex justify-content-between">
-          <Button variant="light" className="projectLinks">
-            <FaGithub className="mr-1" /> Frontend
-          </Button>
-          <Button variant="dark" className="projectLinks">
-            Live
-          </Button>
-          <Button variant="light" className="projectLinks">
-            <FaGithub className="mr-1" /> Backend
-          </Button>
+        <div className="d-flex flex-column justify-content-between">
+          <Card.Text>{props.description}</Card.Text>
+          <div className="d-flex justify-content-between">
+            <Button variant="light" className="projectLinks">
+              <FaGithub className="mr-1" /> Frontend
+            </Button>
+            <Button variant="outline-light" className="projectLinks px-2">
+              Live
+            </Button>
+            <Button variant="light" className="projectLinks">
+              <FaGithub className="mr-1" /> Backend
+            </Button>
+          </div>
         </div>
       </Card.Body>
       <Card.Footer>
